2c3ff6f724f14c022d1c2c5a8f25480d2d6499e5
Flavio Pinto <flaviopnt@hotmail.it>
2020-06-11 17:38:33 +0200

    Jumbotron markup

--- resources/views/static-pages/index.blade.php
+++ resources/views/static-pages/index.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.main')
 
 @section('main-content')
-    <h1>Home Page</h1>
+    <div class="main__jumbotron">
+        <div class="main__jumbotron__left">
+            <h1 class="main__jumbotron__left__title">DIVENTA SVILUPPATORE WEB</h1>
+            <p class="main__jumbotron__left__objective">Trasformiamo la tua passione in una carriera. Se non trovi lavoro, ti rimborsiamo.</p>
+            <ul class="main__jumbotron__left__offer">
+                <li class="main__jumbotron__left__offer__item">6 mesi di corso intensivo online in diretta</li>
+                <li class="main__jumbotron__left__offer__item">Nessuna competenza di programmazione richiesta</li>
+                <li class="main__jumbotron__left__offer__item">Siamo certi del tuo successo, altrimenti ti rimborsiamo</li>
+            </ul>
+        </div>
+        <div class="main__jumbotron__right">
+            <img src="" alt="" class="main__jumbotron__right__img">
+        </div>
+    </div>
 @endsection
