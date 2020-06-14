f89d9ddc3c55e8f62550b87853960bc7cccd4ed4
Flavio Pinto <flaviopnt@hotmail.it>
2020-06-14 21:46:21 +0200

    Header nav

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -4,20 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Boolean Laravel</title>
 </head>
 <body>
     <header class="header">
         <nav class="header__nav">
-            <a href="{{route('static-page.home')}}" class="header__nav__brand">Boolean Placeholder <img class="header__nav__brand__img" src="" alt=""></a>
+            <a href="{{route('static-page.home')}}" class="header__nav__brand"><img class="header__nav__brand__img" src="{{asset('img/boolean-logo.png')}}" alt="boolean logo"></a>
             <ul class="header__nav__list">
-                <li class="header__nav__list__item"><a href="{{route('static-page.home')}}">Home</a></li>
-                <li class="header__nav__list__item"><a href="#">Corso</a></li>
-                <li class="header__nav__list__item"><a href="{{route('students.index')}}">Dopo il corso</a></li>
-                <li class="header__nav__list__item"><a href="#">Lezione gratuita</a></li>
-                <li class="header__nav__list__item"><a href="#">Assumi i nostri studenti</a></li>
-                <li class="header__nav__list__item"><a href="#">Candidati ora</a></li>
+                <li class="header__nav__list__item @if(Request::route()->getName() == 'static-page.home') active @endif"><a class="header__nav__list__item__link" href="{{route('static-page.home')}}">Home</a></li>
+                <li class="header__nav__list__item"><a class="header__nav__list__item__link" href="#">Corso</a></li>
+                <li class="header__nav__list__item @if(Request::route()->getName() == 'students.index') active @endif"><a class="header__nav__list__item__link" href="{{route('students.index')}}">Dopo il corso</a></li>
+                <li class="header__nav__list__item"><a class="header__nav__list__item__link" href="#">Lezione gratuita</a></li>
+                <li class="header__nav__list__item"><a class="header__nav__list__item__link" href="#">Assumi i nostri studenti</a></li>
+                <li class="header__nav__list__item"><a class="header__nav__list__item__link" href="#">Candidati ora</a></li>
             </ul>
         </nav>
     </header>
